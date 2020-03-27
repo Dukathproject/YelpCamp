fd4aa600cec17fe0fd7d0fd378f1e93dfcd51956
Dukathproject <dukathproject@github.com>
2020-03-27 00:43:32 -0300

    Add basic functionality

---
+++ v1/app.js
@@ -0,0 +1,64 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+
+//temporal array
+var campgrounds = [
+    {name: "Salmon Creek", image:"https://images.unsplash.com/photo-1533873984035-25970ab07461?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80"},
+    {name: "Granite Hill", image:"https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
+    {name: "Mountain Goat's Rest", image:"https://images.unsplash.com/photo-1525811902-f2342640856e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"}
+];
+
+
+//ejs extension cut
+app.set("view engine", "ejs");
+
+//use body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+//ROUTES
+
+//home
+app.get("/", function(req, res){
+    res.render("landing")
+});
+
+//view all campgrounds
+app.get("/campgrounds", function(req, res){
+    res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+//submit campground
+app.get("/campgrounds/new", function(req, res){
+    res.render("new");
+});
+
+
+
+
+//posts
+app.post("/campgrounds", function(req, res){
+    //get data from the form
+    var name = req.body.name;
+    var image = req.body.image;
+
+    //push data to the database from a new object to the database array
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    
+    //finally redirect to campgrounds to show added campground
+    res.redirect("/campgrounds");
+
+});
+
+
+
+
+//server listen
+app.listen(3000, function(req, res) {
+    console.log("YelpCamp server has started!");
+});
